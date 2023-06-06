07e1a4b8a6392216f75e44fedcf61b97cd9730b6
mettle001 <540435039@qq.com>
2023-06-06 13:29:42 +0800

    测试2

--- app/src/main/java/com/example/gitandroidk/MainActivity.java
+++ app/src/main/java/com/example/gitandroidk/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.seshi.CynText;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
